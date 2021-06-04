1d1274c909e25734af743072801a3228e8b8b6d2
naluwan <street.baller06@gmail.com>
2021-06-04 18:27:06 +0800

    feat: add Cookie-based Authentication

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+let isLogin = false
 
 router.get('/', (req, res) => {
+
+  isLogin = false
+  if (req.signedCookies.userName && req.signedCookies.userEmail) {
+    isLogin = true
+    return res.render('welcome', { userName: req.signedCookies.userName, isLogin })
+
+  }
+
   res.render('index')
 })
 
